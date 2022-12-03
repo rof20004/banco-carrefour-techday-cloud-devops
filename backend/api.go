85b1715e2b9a109b69e86aa165cb2ea2bbb70eb0
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2022-12-03 13:34:28 -0400

    refactor: alteração de estrutura da aplicação. Reorganização do código

---
+++ backend/api.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type CreateCommentRequest struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Comment string `json:"comment"`
+}
+
+func initializeRestfulServer() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/comments", createComment)
+
+	log.Println("Server started at port 8080")
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Println(err)
+	}
+}
+
+func createComment(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	enableCors(&w)
+
+	defer closeRequestBody(r)
+
+	var payload CreateCommentRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := saveComment(&payload); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseData, err := json.Marshal(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseData)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization")
+}
+
+func closeRequestBody(r *http.Request) {
+	_ = r.Body.Close()
+}
---
+++ backend/database.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+var (
+	dbConn   *sql.DB
+	dbDriver = "mysql"
+	dbHost   = os.Getenv("DB_HOST")
+	dbPort   = os.Getenv("DB_PORT")
+	dbUser   = os.Getenv("DB_USER")
+	dbPass   = os.Getenv("DB_PASS")
+	dbName   = os.Getenv("DB_NAME")
+)
+
+func initializeDatabase() {
+	connString := fmt.Sprintf("%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	db, err := sql.Open(dbDriver, connString)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+
+	dbConn = db
+}
+
+func saveComment(comment *CreateCommentRequest) error {
+	comment.Id = uuid.NewString()
+
+	if _, err := dbConn.Exec("INSERT INTO mensagens(id, name, email, comment) VALUES(?, ?, ?, ?)", comment.Id, comment.Name, comment.Email, comment.Comment); err != nil {
+		return err
+	}
+
+	return nil
+}
