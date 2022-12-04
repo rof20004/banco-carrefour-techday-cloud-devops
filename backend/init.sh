c1dd15c6f4598a12f60d095fe6aec2871cb29a12
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2022-12-04 02:51:48 -0400

    doc: adicionado informações sobre o script de inicialização do backend

--- backend/init.sh
+++ backend/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Esse script é utilizado no Dockerfile para inicializar os dois processos no container Docker: aplicação em go e o nginx
+
 # Start api
 nohup ./backend > backend.log 2>&1 &
 
