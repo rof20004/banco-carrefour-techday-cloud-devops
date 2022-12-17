d2161e97f5b346918980dc57a9281137a45a4dab
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2022-12-17 16:23:38 -0400

    refactor: removido Nginx das configurações do backend

--- backend/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Esse script é utilizado no Dockerfile para inicializar os dois processos no container Docker: aplicação em go e o nginx
-
-# Start api
-nohup ./backend > backend.log 2>&1 &
-
-# Start nginx
-nginx -g 'daemon off;'
